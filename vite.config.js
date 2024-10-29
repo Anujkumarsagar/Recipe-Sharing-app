649319a0cbee522da8dd0bda046ae714a700edeb
AnujKumar <109503594+Anujkumarsagar@users.noreply.github.com>
2024-10-29 22:30:25 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: import.meta.env.MODE === 'development' ? {
+    proxy: import.meta.env.VITE_MODE === 'development' ? {
       '/api': {
         target: import.meta.env.VITE_BACKEND_URL, // Custom environment variable
         changeOrigin: true,
